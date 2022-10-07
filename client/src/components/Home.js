8965e86000f79e47a4e099456ebca4b03660c45b
mkravos <mikekra@outlook.com>
2022-10-07 14:38:50 -0500

    navigation

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -11,15 +11,18 @@ export default function Home() {
     if(!renderWelcome) {
         if(!isMobile) {
             return (
-                <header className="App-header">
+                <header className="App-header Content-page">
                     <img className='background' alt='background' src={background}/>
-                    <button className="Back-btn-desktop" onClick={onShow}>
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-arrow-left-short" viewBox="0 0 16 16">
-                            <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
-                        </svg>
-                    </button>
+                    <div className="Location-bar">
+                        <button className="Back-btn-desktop" onClick={onShow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                            </svg>
+                        </button>
+                        <div className="location">About Me</div>
+                    </div>
                     <div className="Home-content-desktop">
-                        Home
+                        
                     </div>
                 </header>
             );
@@ -27,13 +30,21 @@ export default function Home() {
             return (
                 <header className="App-header">
                     <img className='background' alt='background' src={background}/>
-                    <button className="Back-btn-mobile" onClick={onShow}>
+                    {/* <button className="Back-btn-mobile" onClick={onShow}>
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-arrow-left-short" viewBox="0 0 16 16">
                             <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
                         </svg>
-                    </button>
+                    </button> */}
+                    <div className="Location-bar">
+                        <button className="Back-btn-mobile" onClick={onShow}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi-arrow-left-short" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z"/>
+                            </svg>
+                        </button>
+                        <div className="location">About Me</div>
+                    </div>
                     <div className="Home-content-mobile">
-                        Home
+                        
                     </div>
                 </header>
             );
