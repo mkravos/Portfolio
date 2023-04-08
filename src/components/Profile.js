e77ee889328a8b80b7c62ac644af7004621641e9
mkravos <mikekra@outlook.com>
2023-04-08 11:16:11 -0500

    simplified content management

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -15,6 +15,24 @@ export default function Profile() {
     // const onShowContactForm = React.useCallback(() => setRenderContactForm(true), []);
     const onShowFeaturedProjects = React.useCallback(() => setRenderFeaturedProjects(true), []);
 
+    // page content
+    const name = "Maj Kravos"
+    const location = "Chicago, IL, United States"
+    const social = useState(
+        <>
+            <a href="https://www.linkedin.com/in/majkravos" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://github.com/mkravos" target="_blank" rel="noopener noreferrer">GitHub</a>
+        </>
+    )
+    const aboutMe = "As a seasoned Software Engineer, I have a proven track record of creating high-quality applications from concept to development and deployment. My experience spans across front-end, back-end, mobile, and database development, and I pride myself on being a versatile team player who thrives in collaborative environments. I'm always eager to learn about new technology and tackle new challenges. I'm a self starter skilled at troubleshooting bugs and managing multiple tasks efficiently, and I strive to deliver high-quality results in a timely manner. If you are seeking a reliable and skilled Software Engineer to enhance your team's capabilities, I invite you to contact me to discuss how I can contribute to your organization's success."
+    const skills = "JavaScript, React, Node.js, Python, Go, Java, Android, HTML, CSS, PostgreSQL, Express, Flask, C#, .NET, C++, Docker, AWS, Azure"
+    const work = useState(
+        <>
+            <a className="companyLink" href="https://www.linkedin.com/company/percipia" target="_blank" rel="noopener noreferrer">Percipia</a>- Software Engineer (2023-Present)
+            <a className="companyLink-margin" href="https://www.linkedin.com/company/asar-digital" target="_blank" rel="noopener noreferrer">ASAR Digital</a>- Software Engineer (2022-2023)
+        </>
+    )
+    const education = "Northern Illinois University: Bachelor of Science in Computer Science"
+
     if(!renderWelcome /*&& !renderContactForm*/ && !renderFeaturedProjects) {
         if(!isMobile) {
             return (
@@ -34,9 +52,9 @@ export default function Profile() {
                             <div className="Profile-header-main">
                                 <img className="Profile-photo-desktop" alt="profile" src={profileImage}/>
                                 <div className="Profile-header-text-desktop">
-                                    <div className="Large-mobile-text">Maj Kravos</div>
-                                    <div className="Muted-mobile-text">Chicago, IL, United States</div>
-                                    <div className="Muted-mobile-text"><a href="https://www.linkedin.com/in/majkravos" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://github.com/mkravos" target="_blank" rel="noopener noreferrer">GitHub</a></div>
+                                    <div className="Large-mobile-text">{name}</div>
+                                    <div className="Muted-mobile-text">{location}</div>
+                                    <div className="Muted-mobile-text">{social}</div>
                                 </div>
                             </div>
                             <div className="Profile-header-buttons-desktop">
@@ -48,9 +66,7 @@ export default function Profile() {
                             <center>
                                 <div className="Large-mobile-text">About Me</div>
                                 <p className="Description-desktop-text">
-                                    As a seasoned Software Engineer, I have a proven track record of creating high-quality applications from concept to development and deployment. My experience spans across front-end, back-end, mobile, and database development, and I pride myself on being a versatile team player who thrives in collaborative environments.
-                                    I'm always eager to learn about new technology and tackle new challenges. I'm a self starter skilled at troubleshooting bugs and managing multiple tasks efficiently, and I strive to deliver high-quality results in a timely manner.
-                                    If you are seeking a reliable and skilled Software Engineer to enhance your team's capabilities, I invite you to contact me to discuss how I can contribute to your organization's success.
+                                    {aboutMe}
                                 </p>
                             </center>
                         </div>
@@ -62,22 +78,21 @@ export default function Profile() {
                                         <div className="Subheading-mobile-text">
                                             Skills
                                         </div>
-                                        <p className="Sub-description-mobile-text">JavaScript, React, Node.js, Python, Go, Java, Android, HTML, CSS, PostgreSQL, Express, Flask, C#, .NET, C++, Docker, AWS, Azure</p>
+                                        <p className="Sub-description-mobile-text">{skills}</p>
                                     </div>
                                     <div className="Description-category">
                                         <div className="Subheading-mobile-text">
                                             Work
                                         </div>
                                         <p className="Sub-description-mobile-text">
-                                            <a className="companyLink" href="https://www.linkedin.com/company/percipia" target="_blank" rel="noopener noreferrer">Percipia</a>- Software Engineer (2023-Present)
-                                            <a className="companyLink-margin" href="https://www.linkedin.com/company/asar-digital" target="_blank" rel="noopener noreferrer">ASAR Digital</a>- Software Engineer (2022-2023)
+                                            {work}
                                         </p>
                                     </div>
                                     <div className="Description-category">
                                         <div className="Subheading-mobile-text">
                                             Education
                                         </div>
-                                        <p className="Sub-description-mobile-text">Northern Illinois University: Bachelor of Science in Computer Science</p>
+                                        <p className="Sub-description-mobile-text">{education}</p>
                                     </div>
                                 </div>
                             </center>
@@ -101,9 +116,9 @@ export default function Profile() {
                         <div className="Profile-header-mobile">
                             <img className="Profile-photo-mobile" alt="profile" src={profileImage}/>
                             <div className="Profile-header-text">
-                                <div className="Large-mobile-text">Maj Kravos</div>
-                                <div className="Muted-mobile-text">Chicago, IL, United States</div>
-                                <div className="Muted-mobile-text"><a href="https://www.linkedin.com/in/majkravos" target="_blank" rel="noopener noreferrer">LinkedIn</a> | <a href="https://github.com/mkravos" target="_blank" rel="noopener noreferrer">GitHub</a></div>
+                                <div className="Large-mobile-text">{name}</div>
+                                <div className="Muted-mobile-text">{location}</div>
+                                <div className="Muted-mobile-text">{social}</div>
                                 {/* <button className="Contact-button-mobile" onClick={onShowContactForm}>Contact Me</button> */}
                                 <button className="Projects-button-mobile" onClick={onShowFeaturedProjects}>View My Projects</button>
                             </div>
@@ -112,9 +127,7 @@ export default function Profile() {
                             <center>
                                 <div className="Large-mobile-text">About Me</div>
                                 <p className="Description-mobile-text">
-                                    I am a Software Engineer with a background in creating web applications from concept to development and deployment, with expertise in front-end, back-end, 
-                                    and database development. I enjoy teamwork, learning about new technology, solving problems, and fixing cars. I pride myself in troubleshooting bugs, 
-                                    efficiently managing multiple tasks, and delivering quality results in a timely manner. 
+                                    {aboutMe}
                                 </p>
                             </center>
                         </div>
@@ -124,18 +137,17 @@ export default function Profile() {
                                 <div className="Subheading-mobile-text">
                                     Skills
                                 </div>
-                                <p className="Sub-description-mobile-text">JavaScript, React, Node.js, Python, Go, Java, Android, HTML, CSS, PostgreSQL, Express, Flask, C#, .NET, C++, Docker, AWS, Azure</p>
+                                <p className="Sub-description-mobile-text">{skills}</p>
                                 <div className="Subheading-mobile-text">
                                     Work
                                 </div>
                                 <p className="Sub-description-mobile-text">
-                                    <a className="companyLink" href="https://www.linkedin.com/company/percipia" target="_blank" rel="noopener noreferrer">Percipia</a>- Software Engineer (2023-Present)
-                                    <a className="companyLink" href="https://www.linkedin.com/company/asar-digital" target="_blank" rel="noopener noreferrer">ASAR Digital</a>- Software Engineer (2022-2023)
+                                    {work}
                                 </p>
                                 <div className="Subheading-mobile-text">
                                     Education
                                 </div>
-                                <p className="Sub-description-mobile-text">Northern Illinois University: Bachelor of Science in Computer Science</p>
+                                <p className="Sub-description-mobile-text">{education}</p>
                             </center>
                         </div>
                     </div>
